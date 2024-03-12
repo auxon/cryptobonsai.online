a102a684f5b10fba68dd6481c360bbe9e17013d7
rah <richard.hein@cognilore.com>
2024-03-12 07:44:10 -0400

    Add event listener for bitwork ready event

--- src/Bitwork.test.js
+++ src/Bitwork.test.js
@@ -1,5 +1,8 @@
 const bitwork = require('bitwork')
 const bit = new bitwork({ rpc: { user: "root", pass: "nfhRO+3[" } })
+
+
+
 describe('bitwork operations', () => {
   bit.on("ready", () => {
     // YOUR LOGIC
@@ -17,6 +20,6 @@ describe('bitwork operations', () => {
   bit.on("tx", (tx) => {
     console.log('bitwork tx', tx)
   })
-  while(running) {
+  while(true) {
   }
 });
