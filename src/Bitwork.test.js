43e04a599524243288aa6229af0088b7c8b23cf9
rah <richard.hein@cognilore.com>
2024-03-12 06:13:26 -0400

    Add gh-pages dependency and Bitwork test file

---
+++ src/Bitwork.test.js
@@ -0,0 +1,20 @@
+const bitwork = require('bitwork')
+const bit = new bitwork({ rpc: { user: "root", pass: "nfhRO+3[" } })
+describe('bitwork operations', () => {
+  bit.on("ready", () => {
+    // YOUR LOGIC
+    console.log('bitwork ready')
+  })
+
+  bit.on("error", (err) => {
+    console.log('bitwork error', err)
+  })
+  
+  bit.on("block", (block) => {
+    console.log('bitwork block', block)
+  })
+  
+  bit.on("tx", (tx) => {
+    console.log('bitwork tx', tx)
+  })
+});
